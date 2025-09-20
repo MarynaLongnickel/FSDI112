a701cd8e0710a47cf74731e9322e5157d4facb55
Maryna Longnickel <maryna.longnickel@gmail.com>
2025-09-20 17:08:50 -0500

    wip

--- blog/accounts/urls.py
+++ blog/accounts/urls.py
@@ -1,6 +1,17 @@
-from django.contrib import admin
-from django.urls import path, include
+# from django.contrib import admin
+# from django.urls import path, include
+
+# urlpatterns = [
+#     path('', include('pages.urls')),
+# ]
+
+from django.urls import path
+from django.contrib.auth import views as auth_views
+
+app_name = "accounts"
 
 urlpatterns = [
-    path('', include('pages.urls')),
+    path('login/', auth_views.LoginView.as_view(template_name='registration/login.html'), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
+    # add signup view if you have one
 ]
